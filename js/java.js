352a74532a86b695c43cb188c01ce594396cca27
leeminji <minjis2s2s2@gmail.com>
2022-04-22 11:12:12 +0900

    플러그인을 커스터마이징 할 수 있다.

--- js/java.js
+++ js/java.js
@@ -1,3 +1,26 @@
 window.onload = function(){
-    
+    const swiper = new Swiper('#mainbanner', {
+        // Optional parameters
+        // effect: "fade",
+        //기본값은 스와이퍼
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '#mainbanner .swiper-pagination',
+          clickable: true,
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '#mainbanner .swiper-button-next',
+          prevEl: '#mainbanner .swiper-button-prev',
+        },
+
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+      
+      });
 }
