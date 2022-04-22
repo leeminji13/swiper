cfba6439cec7f81d1e0e8a3eac4c14898fcb94b4
leeminji <minjis2s2s2@gmail.com>
2022-04-22 14:49:02 +0900

    버튼 위치 우측상단으로,색상 수정

--- js/java.js
+++ js/java.js
@@ -1,20 +1,40 @@
 window.onload = function(){
     const swiper = new Swiper('#mainbanner', {
+        // effect: "fade",
+        //기본값은 스와이퍼
+        loop: true,
+        pagination: {
+          el: '#mainbanner .swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '#mainbanner .swiper-button-next',
+          prevEl: '#mainbanner .swiper-button-prev',
+        },
+        // autoplay: {
+        //     delay: 3000,
+        //     disableOnInteraction: false,
+        // },
+      });
+
+      const swiper2 = new Swiper('#secondbanner', {
         // Optional parameters
         // effect: "fade",
         //기본값은 스와이퍼
         loop: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
       
         // If we need pagination
         pagination: {
-          el: '#mainbanner .swiper-pagination',
+          el: '#secondbanner .swiper-pagination',
           clickable: true,
         },
       
         // Navigation arrows
         navigation: {
-          nextEl: '#mainbanner .swiper-button-next',
-          prevEl: '#mainbanner .swiper-button-prev',
+          nextEl: '#secondbanner .swiper-button-next',
+          prevEl: '#secondbanner .swiper-button-prev',
         },
 
         autoplay: {
